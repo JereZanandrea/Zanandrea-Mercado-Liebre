c6202a7e3547a4acf7e6139232f3b7e7df1dcc3a
JereZanandrea <zanandreajeremias@gmail.com>
2024-01-08 18:55:19 -0300

    completo mercado liebre

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const PORT = 3001;
+app.listen(PORT, ()=>{
+    console.log(`Se prendió el puerto ${PORT}`);    
+});
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, "views/home.html"))
+});
+
+app.get("/register", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+});
+
+app.get("/login", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
